42c5300c97d72053cfb2d9f581aa0e09c5d68e39
Keira Brooks <kbrooks@stsci.edu>
2017-08-18 15:38:51 -0400

    Have working run_fgs_commissioning_tools with files going to designated directories

--- run_fgs_commissioning_tool.py
+++ run_fgs_commissioning_tool.py
@@ -4,27 +4,33 @@ import os
 import FGS_commissioning
 import select_psfs
 import nircam_to_fgs
+import utils
 
 #Because Jupyter Notebook cannot open a matplotlib object, I have copied what is
 # done in Run RGS Commissioning Tool.ipynb into this script that should be run in
 # IPython
 local_path = os.path.dirname(os.path.realpath(__file__))
 
-def run_all(nircam_im, guider, root=None, fgs_counts=None, jmag=None,
-            nircam_mod=None):
+def run_all(im, guider, root=None, fgs_counts=None, jmag=None,
+            nircam_mod=None, nircam=True,num_psfs=18):
     if root is None:
-        root = os.path.basename(nircam_im).split('.')[0]
+        root = os.path.basename(im).split('.')[0]
 
     # convert NIRCam image to an FGS image
-    fgs_im = nircam_to_fgs.convert_im(nircam_im, guider, fgs_counts=fgs_counts,
+    if nircam:
+        print("This is a NIRCam image")
+        fgs_im = nircam_to_fgs.convert_im(im, guider, fgs_counts=fgs_counts,
                                       jmag=jmag, nircam_mod=None, return_im=True)
+    else:
+        print("This is a FGS image")
+        fgs_im = utils.read_fits(im)[1]
 
     # create reg file
     output_path = os.path.join(local_path,'out',root)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     nref = select_psfs.create_reg_file(fgs_im,root,guider,output_path=output_path,
-                                       return_nref=True)
+                                       return_nref=True, num_psfs=num_psfs)
     # create all files for FSW/DHAS/FGSES/etc.
     FGS_commissioning.run_ID(fgs_im, guider, root, nref=nref)
     FGS_commissioning.run_ACQ(fgs_im, guider, root)
