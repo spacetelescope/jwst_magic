Normalizing TRK images to 1000 counts; option to normalize FGS images with jmag; fix error with naming input files with '"." in name

FGS_commissioning.py
@@ -98,9 +98,12 @@ class FGS(object):
                                                               skiprows=1).T
 
         # Cover cases where there is only one entry in the reg file
-        self.xarr = np.asarray([self.xarr])
-        self.yarr = np.asarray([self.yarr])
-        self.countrate = np.asarray([self.countrate])
+        try:
+            len(self.xarr)
+        except TypeError:
+            self.xarr = np.asarray([self.xarr])
+            self.yarr = np.asarray([self.yarr])
+            self.countrate = np.asarray([self.countrate])
 
     def get_guide_star_coords(self, gs_ind=0):
         '''
@@ -418,7 +421,14 @@ def convert_fits_to_dat(infile, obsmode, out_dir, root=None):
         data = fits.getdata(infile)
 
         filename = infile.split('/')[-1]
-        root = filename.split('.')[0]
+        filename_split = filename.split('.')
+
+        if len(filename_split) == 2:
+            # If filename is in form ~~~.dtype
+            root = filename_split[0]
+        else:
+            # If filename is in form ~~.~~~.dtype
+            root = '.'.join(filename_split[:-1])
     else:
         root = '{}_{}'.format(root, obsmode)
 
@@ -657,6 +667,9 @@ def create_lostrk(image, guider, root, nx, ny, out_dir=None):
     trk.biasped = False
     trk.poissonnoise = False
     trk.create_arrays(trk.xgs, trk.ygs - trk.yoffset, cds=False)
+
+    # Normalize to a count sum of 1000
+    trk.image = trk.image / np.sum(trk.image) * 1000
 
     # Resize image array to oversample by 6 (from 43x43 to 256x256)
     trk.image = trk.image.repeat(6, axis=1)

run_fgs_commissioning_tool.py
@@ -13,6 +13,7 @@ import log
 import nircam_to_fgs
 import utils
 import background_stars
+import counts_to_jmag
 
 
 # Because Jupyter Notebook cannot open a matplotlib object, I have copied what is
@@ -63,15 +64,23 @@ def run_all(image, guider, root=None, fgs_counts=None, jmag=None,
     log.info("Processing request for {}. \nAll data will be saved in: {}".format(root, out_dir))
     utils.ensure_dir_exists(out_dir)
 
-    # convert NIRCam image to an FGS image
+    # Either convert provided NIRCam image to an FGS image...
     if nircam:
         log.info("This is a NIRCam image")
         fgs_im = nircam_to_fgs.convert_im(image, guider, fgs_counts=fgs_counts,
                                           jmag=jmag, nircam_mod=nircam_mod,
                                           return_im=True)
+
+    # ... or process provided FGS image
     else:
         log.info("This is a FGS image")
         fgs_im = fits.getdata(image)
+
+        # If J magnitude is provided, normalize the entire image to match that jmag
+        if jmag:
+            log.info("Normalizing to jmag = {}".format(jmag))
+            fgs_counts = counts_to_jmag.jmag_to_fgs_counts(jmag, guider)
+            fgs_im = fgs_im / np.sum(fgs_im) * fgs_counts
 
         # Correct high or low pixels
         fgs_im[fgs_im < 0] = 0.