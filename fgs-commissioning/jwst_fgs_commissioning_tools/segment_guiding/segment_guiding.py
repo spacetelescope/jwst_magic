461916c1616c4193900bae5598bf4e9ec6ff43a7
Keira Brooks <kbrooks@stsci.edu>
2018-03-06 16:52:01 -0500

    added default out dir path.. THIS NEEDS TO BE FIXED

--- fgs-commissioning/jwst_fgs_commissioning_tools/segment_guiding/segment_guiding.py
+++ fgs-commissioning/jwst_fgs_commissioning_tools/segment_guiding/segment_guiding.py
@@ -39,6 +39,9 @@ class SegmentGuidingCalculator:
 
         self.root = utils.make_root(root, segment_infile)
         self.out_dir = out_dir
+        if self.out_dir is None:
+            self.out_dir = utils.make_out_dir(self.out_dir, OUT_PATH, root)
+
         utils.ensure_dir_exists(self.out_dir)
 
         # Will the tool be run through the GUI?
