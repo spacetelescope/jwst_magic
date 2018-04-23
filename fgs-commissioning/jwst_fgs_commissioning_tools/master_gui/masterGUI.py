1499a418458af9753f467b5eeb0baaba6da0c210
Lauren Chambers <lchambers@stsci.edu>
2018-04-23 17:55:35 -0400

    JWSTFGS-27; Add image preview (input and converted) to master GUI and auto-load instrument/detector from header

--- fgs-commissioning/jwst_fgs_commissioning_tools/master_gui/masterGUI.py
+++ fgs-commissioning/jwst_fgs_commissioning_tools/master_gui/masterGUI.py
@@ -42,7 +42,7 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QInputDialog,
                              QFileDialog, QLabel, QCheckBox, QFrame, QSizePolicy,
                              QHBoxLayout, QSplitter, QTextEdit, QStyleFactory,
                              QSpinBox, QComboBox, QRadioButton, QGridLayout,
-                             QGroupBox, qApp, QButtonGroup)
+                             QGroupBox, qApp, QButtonGroup, QTabWidget)
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import pyqtSlot, Qt, QSettings, QTimer
@@ -50,10 +50,13 @@ import matplotlib
 if matplotlib.get_backend() != 'Qt5Agg':
     matplotlib.use('Qt5Agg')  # Make sure that we are using Qt5
 from astropy.io import fits
+from astropy.io import ascii as asc
+import numpy as np
 
 from .. import run_fgs_commissioning_tool, utils
 from ..convert_image import counts_to_jmag
 from ..segment_guiding import segment_guiding
+from ..star_selector.SelectStarsGUI import StarClickerMatplotlibCanvas
 
 MASTERGUI_PATH = os.path.dirname(os.path.realpath(__file__))
 PACKAGE_PATH = os.path.split(MASTERGUI_PATH)[0]
@@ -73,7 +76,7 @@ class MasterGui(QMainWindow):
         QMainWindow.__init__(self)
 
         self.title = 'Master GUI - FGS Commissioning Tools'
-        self.image_dim = 1000
+        self.image_dim = 300
         self.app = app
 
         # Set stages
@@ -95,6 +98,8 @@ class MasterGui(QMainWindow):
         self.star_selection_gui_default = star_selection_gui
         self.in_file_default = in_file
         self.out_dir_default = out_dir
+
+        self.converted_im_circles = []
 
         self.initUI()
 
@@ -135,18 +140,22 @@ class MasterGui(QMainWindow):
         segmentGroupBox = self.create_segment_section()
         mainGrid.addWidget(segmentGroupBox, 4, 0, 1, 2)
 
+        # ###  Build the IMAGE PREVIEW section -------------------------------
+        imagepreviewGroupBox = self.create_imagepreview_section()
+        mainGrid.addWidget(imagepreviewGroupBox, 0, 2, 6, 1)
+
         # ###  Save out inputs and run tool ------------------------------------
         button_run = QPushButton("Run", self)
         button_run.resize(button_run.minimumSizeHint())
         button_run.setStyleSheet('QPushButton { background-color: #7DCEA0 }')
         button_run.clicked.connect(self.run_tool)
-        mainGrid.addWidget(button_run, 6, 0, 1, 1)
+        mainGrid.addWidget(button_run, 5, 0, 1, 1)
 
         button_done = QPushButton("Exit", self)
         button_done.resize(button_done.minimumSizeHint())
         button_done.setStyleSheet('QPushButton { background-color: #EC7063 }')
         button_done.clicked.connect(self.close_application)
-        mainGrid.addWidget(button_done, 6, 1, 1, 1)
+        mainGrid.addWidget(button_done, 5, 1, 1, 1)
 
         # Show the GUI ---------------------------------------------------------
         self.show()
@@ -515,6 +524,57 @@ class MasterGui(QMainWindow):
 
         return self.segmentGroupBox
 
+    def create_imagepreview_section(self):
+        '''Show loaded image preview
+        '''
+        imagepreviewGroupBox = QGroupBox('Image Preview', self)
+        imagepreviewGroupBox.setStyleSheet(GROUPBOX_TITLE_STYLESHEET)
+        imagepreviewGrid = QGridLayout()
+        imagepreviewGroupBox.setLayout(imagepreviewGrid)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.input_tab = QWidget()
+        self.converted_tab = QWidget()
+        # self.converted_tab.setEnabled(False)
+
+        # Add tabs
+        self.tabs.addTab(self.input_tab, "Input Image")
+        self.tabs.addTab(self.converted_tab, "Converted Image")
+        # self.tabs.setTabEnabled(0, False)
+        # self.tabs.setTabEnabled(1, False)
+
+        # Create input image tab
+        self.input_tab.layout = QGridLayout()
+        self.input_tab.layout.addWidget(QLabel('Showing Image:', self), 0, 0)
+        self.input_preview_textbox = QTextEdit('No input image loaded.')
+        self.input_preview_textbox.setMaximumSize(2000, 45)
+        self.input_preview_textbox.setEnabled(False)
+        self.input_tab.layout.addWidget(self.input_preview_textbox, 0, 1)
+
+        self.input_canvas = StarClickerMatplotlibCanvas(self.input_tab, data=None, x=None, y=None)
+        self.input_canvas.setMinimumSize(self.image_dim, 0)
+        self.input_tab.layout.addWidget(self.input_canvas, 1, 0, 1, 2)
+        self.input_tab.setLayout(self.input_tab.layout)
+
+        # Create converted image tab
+        self.converted_tab.layout = QGridLayout()
+        self.converted_tab.layout.addWidget(QLabel('Showing Image:', self), 0, 0)
+        converted_im_filepath = os.path.join(self.textbox_outdir.text(), 'out', self.textbox_root.text(), 'FGS_imgs')
+        self.converted_preview_textbox = QTextEdit('No converted image found at {}.'.format(converted_im_filepath))
+        self.converted_preview_textbox.setMaximumSize(2000, 45)
+        self.converted_preview_textbox.setEnabled(False)
+        self.converted_tab.layout.addWidget(self.converted_preview_textbox, 0, 1)
+
+        self.converted_canvas = StarClickerMatplotlibCanvas(self.converted_tab, data=None, x=None, y=None)
+        self.converted_canvas.setMinimumSize(self.image_dim, 0)
+        self.converted_tab.layout.addWidget(self.converted_canvas, 1, 0, 1, 2)
+        self.converted_tab.setLayout(self.converted_tab.layout)
+
+        imagepreviewGrid.addWidget(self.tabs, 0, 0)
+
+        return imagepreviewGroupBox
+
     def close_application(self):
         ''' Close the window'''
         self.close()
@@ -596,8 +656,7 @@ class MasterGui(QMainWindow):
         if self.segmentGroupBox.isChecked():
             refonly = self.cb_refonly.isChecked()
 
-            segment_infile = os.path.join(out_dir, 'out', root,
-                                          '{}_G{}_ALLpsfs.txt'.format(root, guider))
+            segment_infile = self.ALLpsfs
             if not os.path.exists(segment_infile):
                 raise OSError('Provided segment infile {} not found.'.format(segment_infile))
 
@@ -606,7 +665,7 @@ class MasterGui(QMainWindow):
             visit_num = self.textbox_visitnum.text()
 
             if nircam:
-                fgs_image = os.path.join(out_dir, 'out', root, 'FGS_imgs', '{}_G{}.fits'.format(root, guider))
+                fgs_image = self.converted_im_file
                 data = fits.open(fgs_image)[0].data
             else:
                 fgs_image = input_image
@@ -648,12 +707,19 @@ class MasterGui(QMainWindow):
                                                star_selectiongui, file_writer, self.app)
             print("** Run Complete **")
 
+            # Update converted image preview
+            self.update_converted_image_preview()
 
     # What to do when specific buttons are pressed -----------------------------
     def on_click_input(self):
-        ''' Using the Input Image Open button (open file) '''
-        filename = self.open_filename_dialog("NIRCam or FGS image")
+        ''' Using the Input Image Open button (open file)
+        '''
+        # Read selected filename
+        filename = self.open_filename_dialog("NIRCam or FGS image", file_type="FITS files (*.fits)")
         self.textbox_input_im.setText(filename)
+
+        # If the root is still undefined, derive the root from the filename and
+        # assume the default output directory (OUT_PATH)
         if self.textbox_root.text() == "":
             root = utils.make_root(self.root_default, self.textbox_input_im.text())
             self.textbox_root.setText(root)
@@ -663,7 +729,15 @@ class MasterGui(QMainWindow):
             if self.textbox_outdir.text() == OUT_PATH:
                 self.textbox_outdir.setEnabled(True)
 
+        # Update the example filepath (and converted image preview, if possible)
         self.update_filepreview()
+
+        # Show input image preview
+        self.load_input_image_data(filename)
+
+        # Show converted image preview, if possible
+        self.update_converted_image_preview()
+
         return filename
 
     def on_click_out(self):
@@ -676,7 +750,7 @@ class MasterGui(QMainWindow):
 
     def on_click_infile(self):
         ''' Using the Infile Open button (open file) '''
-        filename = self.open_filename_dialog('In/Reg file')
+        filename = self.open_filename_dialog('In/Reg file', file_type="regfile (*.txt)")
         self.textbox_infile.setText(filename)
         return filename
 
@@ -804,29 +878,19 @@ class MasterGui(QMainWindow):
             # self.starGroupBox.setChecked(True)
             self.starGroupBox.setEnabled(True)
 
-
     # What to do with specific buttons ------------------------------------------
     def btnstate(self, button):
         ''' Simple button checking code. Only needs to do something specific
         for choosing an FGS image vs NIRCam image.'''
-        if button.text() == "FGS Image":
-            if button.isChecked():
-                self.cb_nircam_det.setEnabled(False)
-            else:
-                self.cb_nircam_det.setEnabled(True)
-        if button.text() == "NIRCam Image":
-            if button.isChecked():
-                self.cb_nircam_det.setEnabled(True)
-            else:
-                self.cb_nircam_det.setEnabled(False)
+        self.cb_nircam_det.setEnabled(self.rb_nircam.isChecked())
 
     # Dialog Boxes -------------------------------------------------------------
-    def open_filename_dialog(self, title):
+    def open_filename_dialog(self, title, file_type="All Files (*)"):
         ''' Dialog box for opening a NIRCam of FGS image'''
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
+        # options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self, "Open {}".format(title),
-                                                  "", "All Files (*);;Python Files (*.py)",
+                                                  "", file_type,
                                                   options=options)
         if fileName:
             return fileName
@@ -835,22 +899,175 @@ class MasterGui(QMainWindow):
         ''' Dialog box for opening a directory'''
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        dirName = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        dirName = QFileDialog.getExistingDirectoryUrl(self, "Select Directory").toString()[7:]
         if dirName:
             return dirName
 
     # Helper Functions ---------------------------------------------------------
     def update_filepreview(self):
+        # If the root, out_dir, and guider have been defined, show an example filepath
+        # to a simulated image, and auto-populate the regfile.text filepath.
+        # Also, auto-generate important filenames as class attributes
         if self.textbox_outdir.text() != "" and self.textbox_root.text() != ""\
            and self.cb_guider.currentText() != "-Select-":
-            self.textbox_filepreview.setText(os.path.join(self.textbox_outdir.text(),
-                                             'out', self.textbox_root.text(), 'dhas',
-                                             '{}_G{}_strips.fits'.format(self.textbox_root.text(),
-                                                                         self.cb_guider.currentText())))
-            self.textbox_infile.setText(os.path.join(self.textbox_outdir.text(),
-                                             'out', self.textbox_root.text(),
-                                             '{}_G{}_regfile.txt'.format(self.textbox_root.text(),
-                                                                         self.cb_guider.currentText())))
+            # Determine root directory
+            root_dir = os.path.join(self.textbox_outdir.text(), 'out',
+                                    self.textbox_root.text())
+
+            # Update example filepath textbox
+            self.textbox_filepreview.setText(
+                os.path.join(root_dir, 'dhas',
+                             '{}_G{}_strips.fits'.format(self.textbox_root.text(),
+                                                         self.cb_guider.currentText()))
+                )
+
+            # Update regfile.txt filepath
+            self.regfile = os.path.join(root_dir,
+                                        '{}_G{}_regfile.txt'.format(self.textbox_root.text(),
+                                                                    self.cb_guider.currentText()))
+            self.textbox_infile.setText(self.regfile)
+
+            # Update ALLpsfs.txt filepath
+            self.ALLpsfs = os.path.join(root_dir,
+                                        '{}_G{}_ALLpsfs.txt'.format(self.textbox_root.text(),
+                                                                    self.cb_guider.currentText()))
+
+            # Update converted FGS image filepath
+            self.converted_im_file = os.path.join(root_dir, 'FGS_imgs',
+                                                  '{}_G{}.fits'.format(self.textbox_root.text(),
+                                                                       self.cb_guider.currentText()))
+
+            # If possible, show converted image preview, too
+            self.update_converted_image_preview()
+
+    def load_input_image_data(self, filename):
+        # Does the input image exist? If so, show it!
+        if os.path.exists(filename) and filename.endswith('.fits'):
+            # Switch to "Input Image" tab
+            self.tabs.setCurrentIndex(0)
+
+            # Load data and prep for showing on log scale
+            with fits.open(filename) as hdulist:
+                if len(hdulist) > 1:
+                    data = hdulist[1].data
+                else:
+                    data = hdulist[0].data
+                header = hdulist[0].header
+            data[data <= 0] = 1
+
+            # Parse instrument
+            try:
+                instrument = header['INSTRUME'].upper()
+                if "NIRCAM" in instrument:
+                    self.rb_nircam.setChecked(True)
+                elif "FGS" in instrument:
+                    self.rb_fgs.setChecked(True)
+            except KeyError:
+                pass
+
+            # Parse detector
+            try:
+                detector = header['DETECTOR'].upper()
+                if "NRC" in detector:
+                    detector = detector.strip()[-2:]
+                    index = self.cb_nircam_det.findText(detector, Qt.MatchFixedString)
+                    if index >= 0:
+                        self.cb_nircam_det.setEnabled(True)
+                        self.cb_nircam_det.setCurrentIndex(index)
+                elif "GUIDER" in detector:
+                    self.rb_fgs.setChecked(True)
+            except KeyError:
+                self.cb_nircam_det.setCurrentIndex(0)
+
+            # Plot data
+            self.input_canvas.compute_initial_figure(self.input_canvas.fig, data,
+                                                     None, None, xlabel='X [pixels]',
+                                                     ylabel='Y [pixels]')
+
+            # Update filepath in textbox
+            self.input_preview_textbox.setEnabled(True)
+            self.input_preview_textbox.setText(filename)
+
+        # Otherwise, show nothing
+        else:
+            # Update textbox showing filepath
+            self.input_preview_textbox.setText('No input image found at {}.'.format(filename))
+            self.input_preview_textbox.setEnabled(False)
+
+            # Update plot to not show anything
+            dummy_img = self.input_canvas.axes.imshow(
+                np.array([[1e4, 1e4], [1e4, 1e4]]), cmap='bone', clim=(1e-1, 1e2)
+            )
+
+        self.input_canvas.draw()
+
+    def update_converted_image_preview(self):
+        # Are all the necessary fields filled in? If not, don't even try.
+        if not (self.textbox_outdir.text() != "" and self.textbox_root.text() != ""\
+           and self.cb_guider.currentText() != "-Select-"):
+            return
+
+        # Does a converted image exist? If so, show it!
+        if os.path.exists(self.converted_im_file):
+            # Prepare to show converted image
+            self.converted_canvas.axes.set_visible(True)
+            # self.tabs.setTabEnabled(1, True)
+            self.tabs.setCurrentIndex(1)
+            self.converted_preview_textbox.setEnabled(True)
+
+            # Update filepath
+            self.converted_preview_textbox.setText(self.converted_im_file)
+
+            # Load data
+            with fits.open(self.converted_im_file) as hdulist:
+                if len(hdulist) > 1:
+                    data = hdulist[1].data
+                else:
+                    data = hdulist[0].data
+            data[data <= 0] = 1
+
+            # Load ALLpsfs.text
+            x, y = [None, None]
+            if os.path.exists(self.ALLpsfs):
+                psf_list = asc.read(self.ALLpsfs)
+                x = psf_list['x']
+                y = psf_list['y']
+
+            # Plot data image and peak locations from ALLpsfs.txt
+            self.converted_canvas.compute_initial_figure(self.converted_canvas.fig, data, x, y)
+
+            # If possible, plot the selected stars in the regfile.txt
+            if os.path.exists(self.regfile):
+                selected_psf_list = asc.read(self.regfile)
+                x_selected = selected_psf_list['x']
+                y_selected = selected_psf_list['y']
+
+                # Remove old circles
+                for line in self.converted_im_circles:
+                    self.converted_canvas.axes.lines.remove(line[0])
+                self.converted_im_circles = [
+                    self.converted_canvas.axes.plot(x_selected[0], y_selected[0],
+                                                    'o', ms=25, mfc='none',
+                                                    mec='yellow', mew=2, lw=0)
+                ]
+                self.converted_im_circles.append(
+                    self.converted_canvas.axes.plot(x_selected[1:], y_selected[1:],
+                                                    'o', ms=25, mfc='none',
+                                                    mec='darkorange', mew=2, lw=0)
+                )
+
+        # If not, show nothing.
+        else:
+            # Update textbox showing filepath
+            self.converted_preview_textbox.setText('No converted guider {} image found at {}.'.format(self.cb_guider.currentText(), self.converted_im_file))
+            self.converted_preview_textbox.setEnabled(False)
+
+            # Update plot to not show anything
+            dummy_img = self.converted_canvas.axes.imshow(
+                np.array([[1e4, 1e4], [1e4, 1e4]]), cmap='bone', clim=(1e-1, 1e2)
+            )
+
+        return self.converted_canvas.draw()
 
 
 #===============================================================================
