c5c6b44ddda30bfd203052689c4d4d947c9a0a18
Lauren Chambers <lchambers@stsci.edu>
2019-05-28 13:08:09 -0400

    Use np.isclose in test_coordinate_transforms

--- jwst_magic/tests/test_coordinate_transforms.py
+++ jwst_magic/tests/test_coordinate_transforms.py
@@ -10,7 +10,7 @@ Use
     ::
         pytest test_coordinate_transforms.py
 """
-
+import numpy as np
 import pytest
 
 from jwst_magic.utils import coordinate_transforms
@@ -18,17 +18,20 @@ from jwst_magic.utils import coordinate_transforms
 PIXEL_COORDS = (1743.3, 241.9)
 ANGLE_COORDS = (-55.736935, 8.139518)
 
+
 DHAS2Raw_parameters = [(1, (928, 206)),
                        (2, (1156, 207))]
 @pytest.mark.parametrize('guider, correct_conversion', DHAS2Raw_parameters)
 def test_DHAS2Raw(guider, correct_conversion):
     x_raw, y_raw = coordinate_transforms.DHAS2Raw(*ANGLE_COORDS, guider)
-    assert (x_raw, y_raw) == correct_conversion
+    assert (x_raw, y_raw) == correct_conversion, \
+        "Incorrect conversion from DHAS to FGS raw coordinates."
 
 
 def test_Idl2DHAS():
     x_dhas, y_dhas = coordinate_transforms.Idl2DHAS(*ANGLE_COORDS)
-    assert (x_dhas, y_dhas) == (55.736935, 8.139518)
+    assert (x_dhas, y_dhas) == (55.736935, 8.139518), \
+        "Incorrect conversion from ideal to DHAS coordinates."
 
 
 Idl2Raw_parameters = [(1, (882, 1833)),
@@ -36,12 +39,14 @@ Idl2Raw_parameters = [(1, (882, 1833)),
 @pytest.mark.parametrize('guider, correct_conversion', Idl2Raw_parameters)
 def test_Idl2Raw(guider, correct_conversion):
     x_raw, y_raw = coordinate_transforms.Idl2Raw(*ANGLE_COORDS, guider)
-    assert (x_raw, y_raw) == correct_conversion
+    assert (x_raw, y_raw) == correct_conversion, \
+        "Incorrect conversion from ideal to FGS raw coordinates."
 
 
 def test_Raw2Det():
     x_det, y_det = coordinate_transforms.Raw2Det(*PIXEL_COORDS)
-    assert (x_det, y_det) == (241.9, 1743.3)
+    assert (x_det, y_det) == (241.9, 1743.3), \
+        "Incorrect conversion from FGS raw to FGS detector coordinates."
 
 
 Raw2DHAS_parameters = [(1, (-53.70147089878638, -49.332391547578155)),
@@ -49,7 +54,10 @@ Raw2DHAS_parameters = [(1, (-53.70147089878638, -49.332391547578155)),
 @pytest.mark.parametrize('guider, correct_conversion', Raw2DHAS_parameters)
 def test_Raw2DHAS(guider, correct_conversion):
     x_dhas, y_dhas = coordinate_transforms.Raw2DHAS(*PIXEL_COORDS, guider)
-    assert (x_dhas, y_dhas) == correct_conversion
+    assert np.isclose(x_dhas, correct_conversion[0]), \
+        "Incorrect conversion from FGS raw to DHAS coordinates."
+    assert np.isclose(y_dhas, correct_conversion[1]), \
+        "Incorrect conversion from FGS raw to DHAS coordinates."
 
 
 Raw2Idl_parameters = [(1, (53.70147089878638, -49.332391547578155)),
@@ -57,15 +65,27 @@ Raw2Idl_parameters = [(1, (53.70147089878638, -49.332391547578155)),
 @pytest.mark.parametrize('guider, correct_conversion', Raw2Idl_parameters)
 def test_Raw2Idl(guider, correct_conversion):
     x_idealangle, y_idealangle = coordinate_transforms.Raw2Idl(*PIXEL_COORDS, guider)
-    assert (x_idealangle, y_idealangle) == correct_conversion
+    assert np.isclose(x_idealangle, correct_conversion[0]), \
+        "Incorrect conversion from FGS raw to ideal coordinates."
+    assert np.isclose(y_idealangle, correct_conversion[1]), \
+        "Incorrect conversion from FGS raw to ideal coordinates."
+
 
 Raw2Tel_parameters = [(1, (-52.6118059444085, -50.49287779246356)),
                       (2, (-52.97899983353878, 49.02258207773389))]
 @pytest.mark.parametrize('guider, correct_conversion', Raw2Tel_parameters)
 def test_Raw2Tel(guider, correct_conversion):
     v2, v3 = coordinate_transforms.Raw2Tel(*PIXEL_COORDS, guider)
-    assert (v2, v3) == correct_conversion
+    assert np.isclose(v2, correct_conversion[0]), \
+        "Incorrect conversion from FGS raw to V2/V3 coordinates."
+    assert np.isclose(v3, correct_conversion[1]), \
+        "Incorrect conversion from FGS raw to V2/V3 coordinates."
+
 
 def test_convert_boresight_to_v2v3():
     v2v3_offset = coordinate_transforms.nrca3pixel_offset_to_v2v3_offset(-20.4, -140.53)
-    assert v2v3_offset == (-0.638167896, -4.4203823924000005)
+
+    assert np.isclose(v2v3_offset[0], -0.638167896), \
+        "V2 value does not match expected value for boresight offset."
+    assert np.isclose(v2v3_offset[1], -4.4203823924000005), \
+        "V3 value does not match expected value for boresight offset."
