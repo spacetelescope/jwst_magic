24ed5a8b8bd2f30005ad3b7c7c61684811e8d60a
Shannon Osborne <sosborne@stsci.edu>
2020-01-23 12:05:34 -0500

    Update new version

--- jwst_magic/__init__.py
+++ jwst_magic/__init__.py
@@ -1,5 +1,22 @@
 import os
+import pkg_resources
+
 JENKINS = 'jenkins' in os.getcwd()
 if not JENKINS:
     from .masterGUI import run_MasterGui as run_tool_GUI
     from .run_magic import run_all as run_tool
+
+module_path = pkg_resources.resource_filename('jwst_magic', '')
+setup_path = os.path.normpath(os.path.join(module_path, '../setup.py'))
+
+try:
+    with open(setup_path) as f:
+        data = f.readlines()
+
+    for line in data:
+        if 'VERSION =' in line:
+            __version__ = line.split(' ')[-1].replace("'", "").strip()
+
+except FileNotFoundError:
+    print('Could not determine jwst_magic version')
+    __version__ = '0.0.0'
