7ed02e1867bf5e868a636a1779ff5a4fe003dee7
Shannon Osborne <sosborne@stsci.edu>
2021-06-28 17:41:34 -0400

    First pass at updating the coordinate trasnformations for the undistorted raw frame

--- jwst_magic/utils/coordinate_transforms.py
+++ jwst_magic/utils/coordinate_transforms.py
@@ -1,15 +1,22 @@
 """Convert between FGS raw/native frame (pixels), ideal angle frame
 (arcsec), and DHAS frame (arcsec).
 
+Note that the images used in MAGIC are all undistorted, so whenever
+the "raw", "det" or "sci" frames are discussed here, they are not the
+actual frames from the SIAF definition, but are instead an undistorted
+frame where the coodinte system matches the "raw", "det" or "sci" frames
+from the SIAF respectively.
+
 Authors
 -------
     - Lauren Chambers
+    - Shannon Osborne
 
 Use
 ---
     ::
-        from jwst_magic.coordinate_transforms import Raw2Det
-        x_det, y_det = Raw2Det(x_raw, y_raw)
+        from jwst_magic.coordinate_transforms import Raw2Idl
+        x_idl, y_idl = Raw2Idl(x_raw, y_raw)
 """
 
 import pysiaf
@@ -48,33 +55,9 @@ def nrcpixel_offset_to_v2v3_offset(x_offset, y_offset, detector):
     return v2_offset, v3_offset
 
 
-def Raw2Det(x_raw, y_raw):
-    """Pass in X Y pixels in the raw/native frame and get out X Y pixels in the
-    SIAF detector frame
-
-    Parameters
-    ----------
-    x_raw : float
-        X pixels in the raw detector frame
-    y_raw : float
-        Y pixels in the raw detector frame
-
-    Returns
-    -------
-    x_det : float
-        X pixels in the SIAF detector frame
-    y_det : float
-        Y pixels in the SIAF detector frame
-    """
-    # Flip raw axes to get det axes
-    x_det = y_raw
-    y_det = x_raw
-    return x_det, y_det
-
-
 def Raw2Idl(x_raw, y_raw, guider):
-    """Pass in X Y pixels in the raw/native frame and get out X Y angles in the
-    ideal frame
+    """Pass in undistorted X and Y pixels in the raw/native coordinate frame
+     and get out X Y angles in the ideal frame
 
     Parameters
     ----------
@@ -92,27 +75,30 @@ def Raw2Idl(x_raw, y_raw, guider):
     y_idealangle : float
         Y angle (arcsec) in the ideal frame
     """
-    # Flip raw axes to get det axes
-    x_det, y_det = Raw2Det(x_raw, y_raw)
-
     if int(guider) == 1:
         fgs_full = FGS_SIAF['FGS1_FULL']
     elif int(guider) == 2:
         fgs_full = FGS_SIAF['FGS2_FULL']
-
-    # If invalid guider number provided...
     else:
         raise ValueError('Unrecognized guider number: {}'.format(guider))
 
-    # Convert detector frame to ideal frame
-    x_idealangle, y_idealangle = fgs_full.det_to_idl(x_det, y_det)
+    # Convert from RAW -> SCI (just a coordinate origin change, no distortion change)
+    x_sci, y_sci = fgs_full.raw_to_sci(x_raw, y_raw)
+
+    # Shift to the IDL origin location
+    x_idl_pix = x_sci - fgs_full.XSciRef  # TODO TBD ON IF USING THIS VALUE IS OKAY
+    y_idl_pix = y_sci - fgs_full.YSciRef
+
+    # Convert from IDL pixels to idl arcseconds
+    x_idealangle = x_idl_pix * fgs_full.XSciScale
+    y_idealangle = y_idl_pix * fgs_full.YSciScale
 
     return x_idealangle, y_idealangle
 
 
 def Raw2Tel(x_raw, y_raw, guider):
-    """Pass in X Y pixels in the raw/native frame and get out angles in the
-    V2/V3 frame
+    """Pass in undistorted X and Y pixels in the raw/native coordinate frame
+     and get out angles in the V2/V3 frame
 
     Parameters
     ----------
@@ -130,26 +116,18 @@ def Raw2Tel(x_raw, y_raw, guider):
     v3 : float
         Angle (arcsec) in the V3 frame
     """
-    # Flip raw axes to get det axes
-    x_det, y_det = Raw2Det(x_raw, y_raw)
-
     if int(guider) == 1:
         fgs_full = FGS_SIAF['FGS1_FULL']
     elif int(guider) == 2:
         fgs_full = FGS_SIAF['FGS2_FULL']
-
-    # If invalid guider number provided...
     else:
         raise ValueError('Unrecognized guider number: {}'.format(guider))
 
-    # Convert detector frame to V2/V3 frame
-    v2, v3 = fgs_full.det_to_tel(x_det, y_det)
+    # Convert from Raw to IDL
+    x_idl, y_idl = Raw2Idl(x_raw, y_raw, guider)
 
-    # Subtract V2 and V3 references
-    v2ref = fgs_full.V2Ref
-    v3ref = fgs_full.V3Ref
-    v2 -= v2ref  # this is putting the JWST FOV TEL frame, to have the origin on the FGS v2/v3 reference point
-    v3 -= v3ref
+    # Convert from IDL -> TEL (just a coordinate origin change, no distortion change)
+    v2, v3 = fgs_full.idl_to_tel(x_idl, y_idl)
 
     return v2, v3
 
@@ -178,43 +156,17 @@ def Idl2DHAS(x_idealangle, y_idealangle):
 
     return x_dhas, y_dhas
 
-def Idl2Raw(x_idealangle, y_idealangle, guider):
-    """Pass in X and Y angles in the ideal frame and get out X Y pixels in the
-    raw detector frame.
-
-    Parameters
-    ----------
-    x_idealangle : float
-        X angle (arcsec) in the ideal frame
-    y_idealangle : float
-        Y angle (arcsec) in the ideal frame
-    guider : int
-        Which FGS detector to convert for (1 or 2)
-
-    Returns
-    -------
-    x_raw : float
-        X pixels in the raw detector frame
-    y_raw : float
-        Y pixels in the raw detector frame
-    """
-
-    x_dhas, y_dhas = Idl2DHAS(x_idealangle, y_idealangle)
-    x_raw, y_raw = DHAS2Raw(x_dhas, y_dhas, guider)
-
-    return x_raw, y_raw
-
 
 def Raw2DHAS(x_raw, y_raw, guider):
-    """Pass in X and Y pixels in the raw/native frame and get out X and Y angles in
-    the frame DHAS requires.
+    """Pass in undistorted X and Y pixels in the raw/native coordinate frame
+    and get out X and Y angles in the frame DHAS requires.
 
     Parameters
     ----------
     x_raw : float
-        X pixels in the raw detector frame
+        Undistorted X pixels in the raw coordinate frame
     y_raw : float
-        Y pixels in the raw detector frame
+        Undistorted Y pixels in the raw coordinate frame
     guider : int
         Which FGS detector to convert for (1 or 2)
 
@@ -229,70 +181,3 @@ def Raw2DHAS(x_raw, y_raw, guider):
     x_dhas, y_dhas = Idl2DHAS(x_idealangle, y_idealangle)
 
     return x_dhas, y_dhas
-
-
-def DHAS2Raw(x_dhas, y_dhas, guider):
-    """Pass in X and Y angles in the DHAS frame and get out X Y pixels in the
-    raw detector frame
-
-    Parameters
-    ----------
-    x_dhas : float
-        X angle (arcsec) in the DHAS frame
-    y_dhas : float
-        Y angle (arcsec) in the DHAS frame
-    guider : int
-        Which FGS detector to convert for (1 or 2)
-
-    Returns
-    -------
-    x_raw : float
-        X pixels in the raw detector frame
-    y_raw : float
-        Y pixels in the raw detector frame
-    """
-    if int(guider) == 1:
-        fgs_full = FGS_SIAF['FGS1_FULL']
-    elif int(guider) == 2:
-        fgs_full = FGS_SIAF['FGS2_FULL']
-
-    # If invalid guider number provided...
-    else:
-        raise ValueError('Unrecognized guider number: {}'.format(guider))
-
-    # DHAS to ideal
-    x_idealangle = -x_dhas
-    y_idealangle = y_dhas
-
-    # Ideal to detector
-    x_det, y_det = fgs_full.idl_to_det(x_idealangle, y_idealangle)
-
-    # Detector to raw
-    try:
-        x_raw = y_det.astype(int)
-        y_raw = x_det.astype(int)
-    except AttributeError:
-        x_raw = int(y_det)
-        y_raw = int(x_det)
-
-    return x_raw, y_raw
-
-
-def write_to_file(xangle, yangle):
-    """ Write ideal angles to file
-
-    Parameters
-    ----------
-    xangle : float
-        X angle (arcsec)
-    yangle : float
-        Y angle (arcsec)
-    """
-    with open('ideal.tmp', 'w') as f:
-        f.write('index xangle yangle\n')
-        try:
-            len(xangle)
-            for i, (xang, yang) in enumerate(zip(xangle, yangle)):
-                f.write('{} {} {}\n'.format(i, xang, yang))
-        except TypeError:
-            f.write('0 {} {}\n'.format(xangle, yangle))
