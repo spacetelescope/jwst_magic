9b46f3071b773c705f5561bad93b4e7b74c3f07b
Keira Brooks <kbrooks@stsci.edu>
2021-12-13 16:14:14 -0500

    Move to using a class for PSF analysis because why didn't I do this sooner

--- notebooks/Commissioning_Support/notebook_utils.py
+++ notebooks/Commissioning_Support/notebook_utils.py
@@ -22,6 +22,235 @@ GA_PSF_LOCATIONS = {'A1-1':[1037, 599], 'A2-2':[1391, 808], 'A3-3':[1375, 1218],
                     'C1-8':[1394, 404], 'C2-10':[1748, 1024], 'C3-12':[1370, 1637],
                     'C4-14':[650, 1630], 'C5-16':[309, 987], 'C6-18':[690, 391]
                     }
+NIRCAM_SW_PIXELSCALE = 0.031 # arcsec/pixel
+
+class PsfAnalysis():
+    def __init__(self, car, image, header, pixelscale, target_location):
+        self.car = car
+        self.image = image
+        self.header = header
+        if pixelscale is None:
+            self.pixelscale = NIRCAM_SW_PIXELSCALE
+        else:
+            self.pixelscale = pixelscale
+
+        self.target_location = target_location
+
+        self.check_header_for_pixelscale()
+        self.corrected_image = correct_pixel_values(self.image)
+
+        # Set up attributes that will be used in the other methods
+        self.info_df = [] # The data frame holding the PSF information
+        self.psfs = [] # List of cutout PSFs
+        self.psf_window_radius = None # The radius of the box used for cutting out PSFs
+        self.ee_fns = None # list of EE interpolation functions
+
+    def check_header_for_pixelscale(self):
+        """The measure_ee function requires the PIXELSCL header keyword
+        """
+        if 'PIXELSCL' not in self.header.keys() and self.pixelscale is not None:
+            self.header['PIXELSCL'] = self.pixelscale
+        elif 'PIXELSCL' in self.header.keys() and self.pixelscale is None:
+            self.pixelscale = self.header['PIXELSCL']
+
+    def get_psf_characteristics(self, x_list, y_list, psf_window_radius=40):
+        '''
+        Get the FWHM in x and y and the ee function for each identified PSF
+        '''
+        # Save the radius used to cut out the postage stamp as it will be used later
+        self.psf_window_radius = psf_window_radius
+        # Make sure that if we don't have the pixelscale value at this point, that the
+        #  user knows that we can't measure the EE
+        if 'PIXELSCL' not in self.header.keys():
+            print('EE cannot be measured because there is no pixelscale information.')
+
+        fwhm_xs = []
+        fwhm_ys = []
+        ee_fns = []
+        for (x, y) in zip(x_list, y_list):
+            # Cut out each measured PSF
+            cutout = self.corrected_image[int(y)-psf_window_radius:int(y)+psf_window_radius,
+                                          int(x)-psf_window_radius:int(x)+psf_window_radius]
+            self.psfs.append(cutout)
+
+            # Measure FWHM
+            fwhm_x, fwhm_y = measure_fwhm(cutout)
+            fwhm_xs.append(fwhm_x)
+            fwhm_ys.append(fwhm_y)
+            # Measure EE
+            try:
+                ee_fn = measure_ee(cutout, self.header)
+            except KeyError:
+                ee_fn = None
+            ee_fns.append(ee_fn)
+
+        return fwhm_xs, fwhm_ys, ee_fns
+
+    def get_image_information(self, smoothing='high', psf_window_radius=40):
+        """
+        Take in the pick_log.txt file from Shadow to load in PSF characteristics. If no pick_log.txt
+        is provided, the locations of the PSFs are found with a MAGIC function and no other PSF
+        characteristics are measured.
+        """
+        # Find each PSF in the input image
+        x_list, y_list = get_position_from_magic(self.corrected_image, smoothing=smoothing)
+        print(f'{len(x_list)} PSFs found')
+        # Get PSF characteristis for each PSF
+        if x_list:
+            fwhm_xs, fwhm_ys, self.ee_fns = self.get_psf_characteristics(x_list,
+                                                                         y_list,
+                                                                         psf_window_radius)
+            self.get_target_location(x_list, y_list)
+            distances_to_target = self.get_distances_to_target(x_list, y_list)
+            # Create the data frame
+            self.info_df = pd.DataFrame(data={'x': x_list, 'y': y_list,
+                                              'fwhm_x': fwhm_xs, 'fwhm_y': fwhm_ys,
+                                              'distance_to_target': distances_to_target})
+        else:
+            self.info_df = None
+
+    def get_target_location(self, x_list, y_list):
+        """ If we don't have a target location in the image, calculate it from the
+        locations of segment PSFs that we do have
+        """
+        if self.target_location is None:
+            self.target_location = (int(np.median(x_list)), int(np.median(y_list)))
+
+    def get_distances_to_target(self, x_list, y_list):
+        """Calculate the distance to the target location for each known PSF
+        """
+        target_x, target_y = self.target_location
+        distances_to_target = distance_to_target_center(x_list, y_list,
+                                                        target_x, target_y)
+        return distances_to_target
+
+    def plot_mosaic_with_psfs(self, xlim=None, ylim=None, label_color='white'):
+        """
+        self.get_psf_information must be run before this method can be used
+        Plot out the mosaic image with the identified PSFs and estimated target location plotted
+        """
+        x_list = self.info_df['x']
+        y_list = self.info_df['y']
+        if xlim is None:
+            xlim = (min(x_list)-100, max(x_list)+100)
+        if ylim is None:
+            ylim = (min(y_list)-100, max(y_list)+100)
+        try:
+            seg_list = self.info_df['segment']
+        except KeyError:
+            seg_list = None
+        # Plot it!
+        plt.figure(figsize=(14, 14))
+        plt.imshow(self.corrected_image, norm=LogNorm(vmin=1, vmax=1000), origin='lower')
+        plt.scatter(self.target_location[0], self.target_location[1], s=500, marker='*', c='white',
+                    edgecolor='C1', label='Estimated Target Location')
+        plt.legend()
+        if x_list is not None and y_list is not None:
+            for j, (x, y) in enumerate(zip(x_list, y_list)):
+                name = seg_list[j] if seg_list is not None else j
+                plt.annotate(name, xy=(x, y), xytext=(x-75, y), c=label_color, fontsize=14,
+                             weight='bold')
+            plt.xlim(xlim)
+            plt.ylim(ylim)
+
+        plt.title(f"{self.car} Mosaic")
+        plt.show()
+
+    def plot_each_identified_segment_psf(self, num_psf_per_row=6, labels=None):
+        """
+        Cut each segment out of the mosaic/image provided based on the x and y locations given with
+        x_list, and y_list
+        """
+        if labels is None:
+            labels = np.arange(len(self.psfs))
+
+        rows = len(self.psfs)//num_psf_per_row+1
+        columns = len(self.psfs) if len(self.psfs) < num_psf_per_row else num_psf_per_row
+        _, ax = plt.subplots(rows, columns, figsize=(4*columns, 4*rows))
+
+        i = 0
+        for j in range(rows):
+            for k in range(columns):
+                try:
+                    ax[j, k].imshow(self.psfs[i], origin='lower', norm=LogNorm(vmin=1))
+                    ax[j, k].set_title(f'PSF {labels[i]}')
+                    i += 1
+                except IndexError:
+                    ax[j, k].axis('off')
+        plt.tight_layout()
+        plt.show()
+
+    def plot_multiple_parameters(self, parameters, xs=None, ys=None, xlim=None, ylim=None):
+        """
+        Give a list of parameters that you want to plot information for. This plot will show the
+        value of each at the location of the PSF that is provided in the info_dictionary, but if the
+        parameters xs and ys are provided, those will be used instead. When we are plotting the PSFs
+        in their GA location, we must provide the GA xs and ys in order to overide the position of
+        each PSF in the original image.
+        """
+        # Check that all parameters are in info_dictionary
+        parameters = list(set(parameters) & set(list(self.info_df.columns)))
+        if not parameters:
+            print('Requested parameters are not in the povided data frame. Nothing to plot')
+        else:
+            fig, ax = plt.subplots(1, len(parameters), figsize=(6*len(parameters), 8))
+            try:
+                segs = self.info_df['segment'][self.info_df['segment'] != 'Unknown'].values
+            except KeyError:
+                segs = np.arange(len(self.info_df))
+
+            if xs is None and ys is None:
+                xs = self.info_df['x'].values
+                ys = self.info_df['y'].values
+
+            for i, param in enumerate(parameters):
+                axs = ax if len(parameters) == 1 else ax[i]
+                for seg, x, y in zip(segs, xs, ys):
+                    axs.annotate(seg, (x, y), (x+30, y+30), fontsize=16)
+                try:
+                    values = self.info_df[param][self.info_df['segment'] != 'Unknown'].values
+                except KeyError:
+                    values = self.info_df[param].values
+
+                pl = axs.scatter(xs, ys, marker='o', s=100, c=values, cmap=cm.RdYlGn_r)
+                fig.colorbar(pl, ax=axs, orientation='horizontal')
+                axs.set_title(f"{param} for each PSF in the image", fontsize=16)
+
+                if xlim and ylim:
+                    plt.xlim(xlim)
+                    plt.ylim(ylim)
+
+    def plot_ee(self, num_psf_per_row=3, labels=None):
+        '''If we have EE measurements, plot them
+        '''
+        if self.pixelscale is not None:
+            ee_pixel_radius = self.psf_window_radius
+            ee_arcsec_radius = ee_pixel_radius * self.pixelscale
+            xs = np.linspace(0, ee_arcsec_radius)
+
+            num_data_points = len(self.ee_fns)
+            if labels is None:
+                labels = np.arange(num_data_points)
+
+            # Just double check that we have an EE function for this PSF
+            rows = num_data_points//num_psf_per_row+1
+            columns = num_data_points if num_data_points < num_psf_per_row else num_psf_per_row
+            _, ax = plt.subplots(rows, columns, figsize=(4*columns, 4*rows))
+
+            i = 0
+            for j in range(rows):
+                for k in range(columns):
+                    try:
+                        ax[j, k].plot(xs, self.ee_fns[i](xs))
+                        ax[j, k].set_title(f'PSF {labels[i]}')
+                        ax[j, k].set_xlabel("Radius [arcsec]")
+                        ax[j, k].set_ylabel("Encircled Energy")
+                        ax[j, k].set_ylim(None, 6e6)
+                        i += 1
+                    except IndexError:
+                        ax[j, k].axis('off')
+            plt.tight_layout()
+            plt.show()
 
 
 def correct_pixel_values(image):
@@ -38,11 +267,11 @@ def correct_pixel_values(image):
 
 def distance_to_target_center(x, y, truth_x, truth_y):
     '''
-    calculate the distance from the PSF in OTE-01 to it's location in OTE-06/LOS-02
+    Calculate the distance from a segment PSF to the expected location of the target in the sky.
     truth_x and truth_y are the location of the boresight
     '''
 
-    return np.sqrt((truth_x - x)**2 + (truth_y - y)**2)
+    return np.sqrt((truth_x - np.asarray(x))**2 + (truth_y - np.asarray(y))**2)
 
 
 def get_position_from_magic(image, smoothing='default', npeaks=np.inf):
@@ -105,74 +334,6 @@ def measure_ee(array, header):
     return poppy.measure_ee(new_hdul, normalize='None')
 
 
-def get_psf_characteristics(data, header, x_list, y_list, radius=36):
-    '''
-    Get the FWHM in x and y and the ee function for each identified PSF
-    '''
-    if 'PIXELSCL' not in header.keys():
-        print('EE cannot be measured because there is no pixelscale information.')
-
-    fwhm_xs = []
-    fwhm_ys = []
-    ee_fns = []
-    psfs = []
-    for i, (x, y) in enumerate(zip(x_list, y_list)):
-        # Cut out each measured PSF
-        cutout = data[int(y)-radius:int(y)+radius,
-                      int(x)-radius:int(x)+radius]
-        psfs.append(cutout)
-
-        # Measure FWHM
-        fwhm_x, fwhm_y = measure_fwhm(cutout)
-        fwhm_xs.append(fwhm_x)
-        fwhm_ys.append(fwhm_y)
-        # Measure EE
-        try:
-            ee_fn = measure_ee(cutout, header)
-        except KeyError:
-            ee_fn = None
-        ee_fns.append(ee_fn)
-
-    return fwhm_xs, fwhm_ys, ee_fns, psfs
-
-
-
-def get_image_information(image, header, smoothing='high'):
-    """
-    Take in the pick_log.txt file from Shadow to load in PSF characteristics. If no pick_log.txt
-    is provided, the locations of the PSFs are found with a MAGIC function and no other PSF
-    characteristics are measured.
-    """
-    x_list, y_list = get_position_from_magic(image, smoothing=smoothing)
-    fwhm_xs, fwhm_ys, ee_fns, psfs = get_psf_characteristics(image, header, x_list, y_list)
-    print(f'Creating the information table from the X and Y values calculated using MAGIC')
-    print(f'{len(x_list)} PSFs found')
-    if x_list:
-        info_df = pd.DataFrame(data={'x': x_list, 'y': y_list,
-                                     'fwhm_x': fwhm_xs, 'fwhm_y': fwhm_ys,
-                                     'ee_fn': ee_fns})
-    else:
-        info_df = None
-
-    return info_df, psfs
-
-
-def get_target_location(target_location, info_df):
-    """ If we don't have a target location in the image, calculate it from the
-    locations of segment PSFs that we do have
-    """
-    if target_location is None:
-        target_x = np.median(info_df['x'].values)
-        target_y = np.median(info_df['y'].values)
-        target_location = (int(target_x), int(target_y))
-    else:
-        target_x, target_y = target_location
-
-    distances_to_target = distance_to_target_center(info_df['x'].values,
-                                                    info_df['y'].values,
-                                                    target_x,
-                                                    target_y)
-    return distances_to_target
 
 
 def convert_df_to_dictionary(info_df):
@@ -215,133 +376,6 @@ def add_segment_to_df(info_df, matching_dictionary):
     return info_df
 
 
-def grab_vmin_vmax(param):
-    """ Give a vmin & vmax depending on what parameter we are plotting
-    """
-    if 'fwhm' in param:
-        vmin = 10 # TBD
-        vmax = 20 # TBD
-        cmap = cm.RdYlGn_r
-    elif param == 'ellipse':
-        vmin = 0
-        vmax = 1
-        cmap = cm.RdYlGn
-    else:
-        vmin = None
-        vmax = None
-        cmap = cm.RdYlGn_r
-
-    return vmin, vmax, cmap
-
-
-def plot_mosaic_with_psfs(mosaic, car, info_df, est_target_location=None,
-                          xlim=None, ylim=None, label_color='white'):
-    """
-    Plot out the mosaic image with the identified PSFs and estimated target location plotted
-    """
-    x_list = info_df['x']
-    y_list = info_df['y']
-
-    if xlim is None:
-        xlim = (min(x_list)-100, max(x_list)+100)
-    if ylim is None:
-        ylim = (min(y_list)-100, max(y_list)+100)
-    try:
-        seg_list = info_df['segment']
-    except KeyError:
-        seg_list = None
-
-    plt.figure(figsize=(14, 14))
-    plt.imshow(mosaic, norm=LogNorm(vmin=1, vmax=1000), origin='lower')
-    if est_target_location is not None:
-        plt.scatter(est_target_location[0], est_target_location[1], s=500, marker='*', c='white',
-                    edgecolor='C1', label='Estimated Target Location')
-        plt.legend()
-    if x_list is not None and y_list is not None:
-        for j, (x, y) in enumerate(zip(x_list, y_list)):
-            name = seg_list[j] if seg_list is not None else j
-            plt.annotate(name, xy=(x, y), xytext=(x-75, y), c=label_color, fontsize=14,
-                         weight='bold')
-        plt.xlim(xlim)
-        plt.ylim(ylim)
-
-    plt.title(f"{car} Mosaic")
-    plt.show()
-
-
-def plot_each_identified_segment_psf(psfs, num_psf_per_row=6, labels=None):
-    """
-    Cut each segment out of the mosaic/image provided based on the x and y locations given with
-    x_list, and y_list
-    """
-    if labels is None:
-        labels = np.arange(len(psfs))
-
-    rows = len(psfs)//num_psf_per_row+1
-    columns = len(psfs) if len(psfs) < num_psf_per_row else num_psf_per_row
-    fig, ax = plt.subplots(rows, columns, figsize=(4*columns, 4*rows))
-
-    i = 0
-    for j in range(rows):
-        for k in range(columns):
-            try:
-                ax[j, k].imshow(psfs[i], origin='lower', norm=LogNorm(vmin=1))
-                ax[j, k].set_title(f'PSF {labels[i]}')
-                i += 1
-            except IndexError:
-                ax[j, k].axis('off')
-
-    plt.show()
-
-
-def plot_multiple_parameters(parameters, info_df, xs=None, ys=None, xlim=None, ylim=None):
-    """
-    Give a list of parameters that you want to plot information for. This plot will show the
-    value of each at the location of the PSF that is provided in the info_dictionary, but if the
-    parameters xs and ys are provided, those will be used instead. When we are plotting the PSFs
-    in their GA location, we must provide the GA xs and ys in order to overide the position of
-    each PSF in the original image.
-
-    """
-    # Check that all parameters are in info_dictionary
-    parameters = list(set(parameters) & set(list(info_df.columns)))
-    if not parameters:
-        print('Requested parameters are not in the povided data frame. Nothing to plot')
-    else:
-        fig, ax = plt.subplots(1, len(parameters), figsize=(6*len(parameters), 8))
-
-        try:
-            segs = info_df['segment'][info_df['segment'] != 'Unknown'].values
-        except KeyError:
-            segs = np.arange(len(info_df))
-
-        if xs is None and ys is None:
-            xs = info_df['x'].values
-            ys = info_df['y'].values
-
-        for i, param in enumerate(parameters):
-            axs = ax if len(parameters) == 1 else ax[i]
-
-            for seg, x, y in zip(segs, xs, ys):
-                axs.annotate(seg, (x, y), (x+15, y+15))
-
-            vmin, vmax, cmap = grab_vmin_vmax(param)
-
-            try:
-                values = info_df[param][info_df['segment'] != 'Unknown'].values
-            except KeyError:
-                values = info_df[param].values
-
-            pl = axs.scatter(xs, ys, marker='o', s=100, c=values, cmap=cmap,
-                            vmin=vmin, vmax=vmax)
-            fig.colorbar(pl, ax=axs, orientation='horizontal')
-            axs.set_title(f"{param} for each PSF in the image")
-
-            if xlim and ylim:
-                plt.xlim(xlim)
-                plt.ylim(ylim)
-
-
 def separate_nircam_images(all_images_list):
     nrca_images = []
     nrcb_images = []
@@ -377,7 +411,7 @@ def plot_nrca_images(data_list, name_list):
     a1, a2, a3, a4 = data_list
     a1_name, a2_name, a3_name, a4_name = name_list
 
-    fig, ax = plt.subplots(2, 2, figsize=(12, 12))
+    _, ax = plt.subplots(2, 2, figsize=(12, 12))
     ax[0, 0].imshow(a2, norm=LogNorm(vmin=1, vmax=1000), origin='lower')
     ax[0, 0].set_title(f'A2: {a2_name}')
     ax[0, 1].imshow(a4, norm=LogNorm(vmin=1, vmax=1000), origin='lower')
